3d9df639ffd0960032c93aa0a3ce108c2e63ba5c
abel <abel@abel-office.(none)>
2012-05-14 17:24:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ModyoCain::Application.config.secret_token = '83f411e72827904c0c1a0633c594f996e22bd757dd9482ddfa53e9e864a301c983b9df0b3bf8087debaf3977c0cb5bce2e9d4ff1a5a41bcc4c99f6793726e34e'
